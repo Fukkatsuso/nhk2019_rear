5f6928766569d2803c6cc79f629768a558684841
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2019-04-10 00:15:08 +0900

    adjust-4.09

--- main.cpp
+++ main.cpp
@@ -211,7 +211,8 @@ void set_cycle(float *period, float *duty){
 		break;
 
 	case MRMode::ReadyForTussock:
-		*period = 200.0/240.0;
+//		*period = 200.0/240.0;
+		*period = 320.0/350.0;
 		break;
 	case MRMode::Tussock:
 		*period = 1;
